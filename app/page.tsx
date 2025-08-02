d39220802fbd3542d02f198985e5f69a1332f0f8
jsgjsg <jsgcys0815@gmail.com>
2025-08-03 01:21:32 +0900

    [study] #3.8 Lists and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,21 @@ export default function Home() {
         className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5"
       >
         <div
-          className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2
-          *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100"
+          className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3"
         >
-          <input
-            className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
-            type="text" placeholder="Email address" required
-          />
-          <span
-            className="text-red-500 font-medium hidden peer-invalid:block"
-          >
-            Email is required.
-          </span>
-          <button
-            className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10 bg-black peer-invalid:bg-red-100 peer-required:bg-green-500"
-          >
-            Log in
-          </button>
+          {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
+            <div key={index} className="flex items-center gap-5">
+              <div className="size-10 bg-blue-400 rounded-full" />
+              <span className="text-lg font-medium empty:w-24 empty:h-4 empty:rounded-full empty:animate-pulse empty:bg-gray-300">{person}</span>
+              <div className="size-6 animate-bounce bg-red-500 text-white flex items-center justify-center rounded-full">
+                <span>{index}</span>
+              </div>
+              <div className="size-6 animate-spin bg-red-500 text-white flex items-center justify-center rounded-full">
+                <span>⌛</span>
+              </div>
+            </div>
+          ))}
+
         </div>
       </main>
   );
