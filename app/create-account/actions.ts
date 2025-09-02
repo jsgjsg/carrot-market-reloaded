a14df2c51e26a4114b591bb837cfe0485346105f
jsgjsg <jsgcys0815@gmail.com>
2025-09-02 23:31:12 +0900

    [study] #6.3 Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,10 @@
 "use server";
 import {z} from "zod";
 
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
+);
+
 const checkUsername = (username:string) => !username.includes("potato");
 
 const checkPasswords = ({ password, confirm_password } : { password: string, confirm_password: string }) => password === confirm_password;
@@ -9,11 +13,14 @@ const formSchema = z.object({
   username: z.string({
     invalid_type_error: "username must be a string",
     required_error: "username is required"
-  }).min(3, "짧아")
+  })
+  .min(3, "짧아")
   .max(10, "길어")
+  .toLowerCase()
+  .trim()
   .refine(checkUsername, "potato is not allowed"),
-  email: z.string().email(),
-  password: z.string().min(10),
+  email: z.string().email().toLowerCase(),
+  password: z.string().min(10).regex(passwordRegex, "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
   confirm_password: z.string().min(10),
 }).refine(checkPasswords, {
   message: "passwords do not match",
