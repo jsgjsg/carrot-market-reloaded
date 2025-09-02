7932c27e6ee392944bcbd30b392423276116b6cd
jsgjsg <jsgcys0815@gmail.com>
2025-09-02 23:56:10 +0900

    [study] #6.4 Refactor

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import FormButton from "@/components/form-btn";
-import FormInput from "@/components/form-input";
+import Button from "@/components/button";
+import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
 import { createAccount } from "./actions";
@@ -16,12 +16,14 @@ export default function CreateAccount() {
             <h2 className="text-xl">Fill in the form below to join!</h2>
           </div>
           <form action={dispatch} className="flex flex-col gap-3">
-            <FormInput name="username" type="text" placeholder="Username" required errors={state?.fieldErrors.username} />
-            <FormInput name="email" type="email" placeholder="Email" required errors={state?.fieldErrors.email} />
-            <FormInput name="password" type="password" placeholder="Password" required errors={state?.fieldErrors.password} />
-            <FormInput name="confirm_password" type="password" placeholder="Confirm Password" required errors={state?.fieldErrors.confirm_password} />
+            <Input
+              name="username" type="text" placeholder="Username" required errors={state?.fieldErrors.username} minLength={3} maxLength={10}
+            />
+            <Input name="email" type="email" placeholder="Email" required errors={state?.fieldErrors.email} />
+            <Input name="password" type="password" placeholder="Password" required errors={state?.fieldErrors.password} minLength={10} />
+            <Input name="confirm_password" type="password" placeholder="Confirm Password" required errors={state?.fieldErrors.confirm_password} minLength={10} />
             
-            <FormButton text="Create account" />
+            <Button text="Create account" />
           </form>
           
           <SocialLogin />
