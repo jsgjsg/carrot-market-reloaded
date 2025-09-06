8840682dd9b82c01bcc1eb4813b90845b6cf06a9
jsgjsg <jsgcys0815@gmail.com>
2025-09-06 22:47:28 +0900

    [study] #6.7 Coerce

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -4,10 +4,10 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
-import { smsVerification } from "./actions";
+import { smsLogin } from "./actions";
 
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerification, null);
+  const [state, dispatch] = useFormState(smsLogin, null);
   return (
       <div className="flex flex-col gap-10 py-8 px-6">
         <div className="flex flex-col gap-2 *:font-medium">
@@ -15,8 +15,8 @@ export default function SMSLogin() {
           <h2 className="text-xl">Verify your phone number.</h2>
         </div>
         <form action={dispatch} className="flex flex-col gap-3">
-          <Input name="phone" type="number" placeholder="Phone number" required />
-          <Input name="token" type="number" placeholder="Verification code" required />
+          <Input name="phone" type="text" placeholder="Phone number" required />
+          <Input name="token" type="number" placeholder="Verification code" required min={100000} max={999999} />
           
           <Button text="Verify" />
         </form>
