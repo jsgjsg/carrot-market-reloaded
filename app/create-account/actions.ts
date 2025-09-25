e3246a40f4e4ca02824951320432811062841eb4
jsgjsg <jsgcys0815@gmail.com>
2025-09-25 23:29:02 +0900

    [study] #8.1 Database Validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,20 +1,46 @@
 "use server";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
-import {z} from "zod";
+import db from "@/lib/db";
+import { z } from "zod";
 
-const checkUsername = (username:string) => !username.includes("potato");
+const checkPasswords = ({ password, confirm_password }: { password: string, confirm_password: string }) => password === confirm_password;
 
-const checkPasswords = ({ password, confirm_password } : { password: string, confirm_password: string }) => password === confirm_password;
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username
+    },
+    select: {
+      id: true
+    }
+  });
+
+  return !Boolean(user);
+}
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email
+    },
+    select: {
+      id: true
+    }
+  });
+
+  return !Boolean(user);
+}
 
 const formSchema = z.object({
   username: z.string({
     invalid_type_error: "username must be a string",
     required_error: "username is required"
   })
-  .toLowerCase()
-  .trim()
-  .refine(checkUsername, "potato is not allowed"),
-  email: z.string().email().toLowerCase(),
+    .toLowerCase()
+    .trim()
+    .refine(checkUniqueUsername, "This username is already taken"),
+  email: z.string().email().toLowerCase()
+    .refine(checkUniqueEmail, "There is an account already registered with that email."),
   password: z.string().min(PASSWORD_MIN_LENGTH).regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
   confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
 }).refine(checkPasswords, {
@@ -22,7 +48,7 @@ const formSchema = z.object({
   path: ["confirm_password"]
 });
 
-export async function createAccount(prevState:any, formData:FormData) {
+export async function createAccount(prevState: any, formData: FormData) {
 
   const data = {
     username: formData.get("username"),
@@ -32,10 +58,13 @@ export async function createAccount(prevState:any, formData:FormData) {
   };
 
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
-  if(!result.success) {
+  if (!result.success) {
     return result.error.flatten();
+  } else {
+
+
   }
 
 }
